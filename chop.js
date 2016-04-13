2ec57939d86de827c8629b435b08539a9f597a0d
david733 <david733@alum.mit.edu>
2016-04-13 13:21:42 -0700

    Simplified chop.js

--- chop.js
+++ chop.js
@@ -6,11 +6,9 @@
  */
 function run(list,chunkSize) {
 	
-	var x = 0;
 	var chunks = [];
 	for (var i=0;i<list.length/chunkSize;i++){
-		chunks.push(list.slice(x,x+chunkSize));
-		x += chunkSize;
+		chunks.push(list.slice(i*chunkSize,(i+1)*chunkSize));
 	}
 	return {chunks:chunks};
 	
