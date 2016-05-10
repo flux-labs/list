813d538e15bb50803c3548f21ddf90ad88e45615
CharlieP <charlie@flux.io>
2016-05-10 11:43:42 -0400

    Formatting
    Changed some formatting for consistency.

--- chop.js
+++ chop.js
@@ -1,17 +1,19 @@
 'use strict';
 
 /**
- * Code block template.
+ * This code block takes a list and splits it into sub lists
+ * @param {List} the original list of values
+ * @param {Size} the size of sub lists
  *
+ * @return {Lists} the new set of lists
  */
-function run(list,chunkSize) {
+function run(List,Size) {
 	
 	var chunks = [];
-	for (var i=0;i<list.length/chunkSize;i++){
-		chunks.push(list.slice(i*chunkSize,(i+1)*chunkSize));
+	for (var i = 0; i < List.length/Size;i++){
+		chunks.push(List.slice(i*Size,(i+1)*Size));
 	}
-	return {chunks:chunks};
-	
+	return {Lists:chunks};
 }
 
 module.exports = {
